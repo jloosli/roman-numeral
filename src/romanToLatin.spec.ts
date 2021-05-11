dfb3fa4748e7e80574a5eb62d94ff14fa0126a72
Jared Loosli <jared-loosli@pluralsight.com>
2021-05-11 17:12:47 -0600

    Ran spec through prettier

--- src/romanToLatin.spec.ts
+++ src/romanToLatin.spec.ts
@@ -1,8 +1,8 @@
-import { romanToLatin } from "./";
+import { romanToLatin } from './'
 
-describe("test latinToRoman function", () => {
-  it("should return a number", function () {
-    const result = romanToLatin("I");
-    expect(typeof result).toBe("number");
-  });
-});
+describe('test latinToRoman function', () => {
+  it('should return a number', function () {
+    const result = romanToLatin('I')
+    expect(typeof result).toBe('number')
+  })
+})
